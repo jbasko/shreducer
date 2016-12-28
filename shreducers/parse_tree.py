0e021e239847bf7785e834d4a6fbae22bb09e892
Jazeps Basko <jazeps.basko@gmail.com>
2016-12-28 21:30:16 +0000

    ParseTreeProcessor and ParseTreeMultiProcessor

--- shreducers/parse_tree.py
+++ shreducers/parse_tree.py
@@ -1,4 +1,11 @@
-class ParseTreeExtras(dict):
+class PtNodeExtras(dict):
+    """
+    A dictionary with attribute-like access.
+    Handy while processing parse tree and you
+    need to store extra information about tree node
+    as you discover it.
+    """
+
     def __missing__(self, key):
         self[key] = None
         return self[key]
@@ -10,14 +17,17 @@ class ParseTreeExtras(dict):
         self[key] = value
 
 
-class ParseTree(object):
+class PtNode(object):
+    """
+    Represents a node in parse tree.
+    """
     __slots__ = ('op', 'a', 'b', 'x')
 
     def __init__(self, *args, **kwargs):
         self.op = None
         self.a = None
         self.b = None
-        self.x = ParseTreeExtras()
+        self.x = PtNodeExtras()
 
         if args:
             if len(args) == 1:
@@ -38,4 +48,110 @@ class ParseTree(object):
             return self.a, self.b
 
     def to_tuple(self):
-        return (self.op,) + tuple(ab.to_tuple() if isinstance(ab, ParseTree) else ab for ab in self.operands)
+        return (self.op,) + tuple(ab.to_tuple() if isinstance(ab, PtNode) else ab for ab in self.operands)
+
+    def __repr__(self):
+        return '<{} op={}, a={}, b={}, x={}>'.format(self.__class__.__name__, self.op, self.a, self.b, self.x)
+
+
+class ParseTreeProcessor(object):
+    """
+    Generic parse tree processor that other processors can extend
+    and just implement custom node processors in methods called
+    `process_<op>` where `<op>` is name of operator.
+
+    To process primitive nodes implement `process_primitive`.
+
+    To catch all non-primitive nodes, implement `process_unrecognised`.
+
+    Make sure each processor does one thing and does it well.
+    If you have a few different things to do before a parse tree can be compiled,
+    you may want to use ParseTreeMultiProcessor which is a chain of processors.
+    """
+
+    class delegate_of(object):
+        """
+        Decorator to allow writing one method that does the job of many.
+        This is handy when you want to process a number of operators with the
+        same code.
+        """
+        def __init__(self, *method_names):
+            self.delegate_of = method_names
+
+        def __call__(self, func):
+            func.delegate_of = self.delegate_of
+            return func
+
+    def __new__(cls, *args):
+        instance = super(ParseTreeProcessor, cls).__new__(cls, *args)
+        for k, v in cls.__dict__.iteritems():
+            if v and callable(v) and hasattr(v, 'delegate_of'):
+                for d in v.delegate_of:
+                    # Must assign the bounded method of instance, not that of the class
+                    setattr(instance, d, getattr(instance, k))
+        return instance
+
+    def process(self, node):
+        if isinstance(node, tuple):
+            node = PtNode(node)
+        if isinstance(node, PtNode):
+            node.a = self.process(node.a)
+            if node.b is not None:
+                node.b = self.process(node.b)
+            return self.do_process(node)
+        else:
+            return self.process_primitive(node)
+
+    def do_process(self, node):
+        processor_name = 'process_{}'.format(node.op)
+        if hasattr(self, processor_name):
+            return getattr(self, processor_name)(node)
+        else:
+            return self.process_unrecognised(node)
+
+    def process_primitive(self, primitive):
+        return primitive
+
+    def process_unrecognised(self, node):
+        return node
+
+
+class ParseTreeMultiProcessor(ParseTreeProcessor):
+    """
+    Represents a chain of processors that need to be applied on each node
+    one after another.
+
+    This class should not be extended as that would complicate thinking about
+    how it works.
+    """
+
+    def __init__(self, *processors):
+        self._processors = processors
+        if self.__class__.__name__ != ParseTreeMultiProcessor.__name__:
+            raise RuntimeError('Attempting to extend {}'.format(ParseTreeMultiProcessor.__name__))
+
+    def _process_with_all(self, method_name, node):
+        for p in self._processors or ():
+            node = getattr(p, method_name)(node)
+        return node
+
+    def process(self, node):
+        if isinstance(node, tuple):
+            node = PtNode(node)
+        if isinstance(node, PtNode):
+            node.a = self._process_with_all(self.process.__name__, node.a)
+            if node.b is not None:
+                node.b = self._process_with_all(self.process.__name__, node.b)
+            return self._process_with_all(self.do_process.__name__, node)
+        else:
+            return self._process_with_all(self.process_primitive.__name__, node)
+
+    def do_process(self, node):
+        raise RuntimeError('Do not call this')
+
+    def process_primitive(self, primitive):
+        raise RuntimeError('Do not call this')
+
+    def process_unrecognised(self, node):
+        raise RuntimeError('Do not call this')
+
--- tests/test_parse_tree.py
+++ tests/test_parse_tree.py
@@ -2,20 +2,20 @@ import copy
 
 import pytest
 
-from shreducers.parse_tree import ParseTree
+from shreducers.parse_tree import PtNode, ParseTreeProcessor, ParseTreeMultiProcessor
 
 
 @pytest.fixture
 def parse_tree():
     return (
         'and',
-        ('or', ('in', 'off', 'tags'), ('eq', 'type', 'special')),
+        ('or', ('in', 'off', 'tags'), ('not', ('eq', 'type', 'special'))),
         ('gt', 'weight', '100'),
     )
 
 
-def test_empty_parse_tree_is_initialised():
-    pt = ParseTree()
+def test_empty_pt_node_is_initialised():
+    pt = PtNode()
     assert pt.op is None
     assert pt.a is None
     assert pt.b is None
@@ -24,8 +24,8 @@ def test_empty_parse_tree_is_initialised():
     assert pt.to_tuple() == (None, None)
 
 
-def test_parse_tree_initialised_from_tuple():
-    pt = ParseTree(('eq', 'type', 'special'))
+def test_pt_node_initialised_from_tuple():
+    pt = PtNode(('eq', 'type', 'special'))
     assert pt.op == 'eq'
     assert pt.a == 'type'
     assert pt.b == 'special'
@@ -33,8 +33,8 @@ def test_parse_tree_initialised_from_tuple():
     assert pt.to_tuple() == ('eq', 'type', 'special')
 
 
-def test_parse_tree_initialised_from_unnamed_args():
-    pt = ParseTree('not', 'this')
+def test_pt_node_initialised_from_unnamed_args():
+    pt = PtNode('not', 'this')
     assert pt.op == 'not'
     assert pt.a == 'this'
     assert pt.b is None
@@ -44,7 +44,7 @@ def test_parse_tree_initialised_from_unnamed_args():
 
 
 def test_parse_tree_initialised_from_kwargs():
-    pt = ParseTree(a='type', b='special', op='eq')
+    pt = PtNode(a='type', b='special', op='eq')
     assert pt.op == 'eq'
     assert pt.a == 'type'
     assert pt.b == 'special'
@@ -55,24 +55,24 @@ def test_parse_tree_initialised_from_kwargs():
 def test_nested_parse_tree_initialised(parse_tree):
     parse_tree_copy = copy.deepcopy(parse_tree)
 
-    pt = ParseTree(parse_tree)
+    pt = PtNode(parse_tree)
     assert pt.op == 'and'
     assert pt.a[0] == 'or'
     assert pt.b[0] == 'gt'
 
-    pt.a = ParseTree(pt.a)
+    pt.a = PtNode(pt.a)
     assert pt.a.op == 'or'
 
-    pt.b = ParseTree(pt.b)
+    pt.b = PtNode(pt.b)
     assert pt.b.op == 'gt'
 
     assert pt.to_tuple() == parse_tree_copy
 
 
-def test_parse_tree_extras(parse_tree):
+def test_pt_node_extras(parse_tree):
     parse_tree_copy = copy.deepcopy(parse_tree)
 
-    pt = ParseTree(parse_tree)
+    pt = PtNode(parse_tree)
     assert pt.x == {}
 
     pt.x.i = 0
@@ -97,3 +97,79 @@ def test_parse_tree_extras(parse_tree):
     assert pt.x.k == 2
 
     assert pt.to_tuple() == parse_tree_copy
+
+
+def test_parse_tree_processor_visits_every_node(parse_tree):
+    class MyProcessor(ParseTreeProcessor):
+        def process_primitive(self, primitive):
+            return '{}!'.format(primitive)
+
+        def process_gt(self, parse_tree):
+            parse_tree.op = 'GREATER_THAN'
+            return parse_tree
+
+        def process_unrecognised(self, parse_tree):
+            parse_tree.op = '{}!'.format(parse_tree.op)
+            return parse_tree
+
+    pt = MyProcessor().process(parse_tree)
+    assert pt.op == 'and!'
+    assert pt.a.op == 'or!'
+    assert pt.b.op == 'GREATER_THAN'
+    assert pt.b.a == 'weight!'
+    assert pt.b.b == '100!'
+
+
+def test_parse_tree_multiprocessor_calls_all_processors(parse_tree):
+    class PrimitivesProcessor(ParseTreeProcessor):
+        def process_primitive(self, primitive):
+            return '{}!'.format(primitive)
+
+    class OperatorsProcessor(ParseTreeProcessor):
+        def process_unrecognised(self, parse_tree):
+            parse_tree.op = '<{}>'.format(parse_tree.op)
+            return parse_tree
+
+    class PostProcessor(ParseTreeProcessor):
+        def process_primitive(self, primitive):
+            return '{}?'.format(primitive)
+
+    proc = ParseTreeMultiProcessor(PrimitivesProcessor(), OperatorsProcessor(), PostProcessor())
+    pt = proc.process(parse_tree)
+
+    t = pt.to_tuple()
+    assert t[0] == '<and>'
+    assert t[1] == ('<or>', ('<in>', 'off!?', 'tags!?'), ('<not>', ('<eq>', 'type!?', 'special!?')))
+    assert t[2] == ('<gt>', 'weight!?', '100!?')
+
+
+def test_delegate_of_decorator_delegates_method(parse_tree):
+    class CustomProcessor(ParseTreeProcessor):
+        @ParseTreeProcessor.delegate_of(
+            'process_in',
+            'process_gt',
+            'process_eq',
+        )
+        def process_custom(self, parse_tree):
+            parse_tree.op = '<{}>'.format(parse_tree.op)
+            return parse_tree
+
+    pt = CustomProcessor().process(parse_tree)
+    t = pt.to_tuple()
+    assert t[0] == 'and'
+    assert t[1] == ('or', ('<in>', 'off', 'tags'), ('not', ('<eq>', 'type', 'special')))
+    assert t[2] == ('<gt>', 'weight', '100')
+
+
+def test_multi_processor_with_no_processors_does_nothing(parse_tree):
+    parse_tree_copy = copy.deepcopy(parse_tree)
+    pt = ParseTreeMultiProcessor().process(parse_tree)
+    assert pt.to_tuple() == parse_tree_copy
+
+
+def test_cannot_extend_multi_processor_cls():
+    class MyMultiProcessor(ParseTreeMultiProcessor):
+        pass
+
+    with pytest.raises(RuntimeError):
+        MyMultiProcessor(ParseTreeProcessor())
